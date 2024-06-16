54bae544e59b1d72904cef50f504200848b49bde
Isaak Mills <isaakmills@gmail.com>
2024-06-16 00:00:38 -0700

    created basic html and css elements. initalized buttons and readout.

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+Goal: create an on screen calculator using javascript and html
+
+*/ 
+
+const body = document.createElement("body");
+document.body.style.background = "linear-gradient(to left,#ff7e5f, #feb47b)";
+const container = document.createElement("container");
+
+const readOut = body.createElement("readOut")
